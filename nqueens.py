e8e6c853634c77b6a57e4ef92974b67c225b76cc
rockers2004 <shashankgosavi2004@gmail.com>
2025-05-06 02:41:24 +0530

    Updated nqueens.py implementation

--- nqueens.py
+++ nqueens.py
@@ -1,19 +1,14 @@
 def is_safe(board, row, col):
     #Bounding : Checking if the column is already occupied
-    for i in range(row):
-        if board[i][col] == 'Q':
+    for i in range(col):
+        if board[row][i] == 'Q':
             return False
         
-    #Bounding: Checking the Left Diagonal
-    max_left = min(row, col)
-    for i in range(1, max_left + 1):
-        if board[row - i][col - i] == 'Q':
+    for j, k in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[j][k] == 'Q':
             return False
-        
-    #Bounding: Checking the Right Diagonal
-    max_right = min(row, len(board) - 1 - col)
-    for i in range(1, max_right + 1):
-        if board[row - i][col + i] == 'Q':
+    for j, k in zip(range(row, len(board)), range(col, -1, -1)):
+        if board[j][k] == 'Q':
             return False
         
     return True
